9d5e551511f692265e9bf99874604fe07a4c771a
Timo <artur.fiedler54@gmail.com>
2019-05-24 00:29:03 +0200

    fix error

--- modules/showChannel.js
+++ modules/showChannel.js
@@ -1,11 +1,12 @@
 const formatDate = require("./formatDate").run;
+const { getTime } = require("../structures/iomireaClient");
 
 module.exports = {
     run: async (channel, client, state = 0) => {
         if (channel.constructor.name === "Number") channel = client.channels.get(channel);
         const messages = state === 0 ? await channel.fetchMessages(true) : channel.messages;
         for(let i=0;i<messages.length;++i) {
-            const time = formatDate(Client.getTime(messages[i].id));
+            const time = formatDate(getTime(messages[i].id));
             console.log("[" + time + "] " + (" ".repeat(15 - time.length)) + "│ " + messages[i].author.name + (" ".repeat(10 - messages[i].author.name.length)) + "│ " + messages[i].content.substr(0, process.stdout.columns || 2048));
         }
         const spacePad = state === 2 ? 3 : 2;
