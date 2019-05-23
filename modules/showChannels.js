31d89954b12a04c09663c02e78293cb61f800d03
Timo <artur.fiedler54@gmail.com>
2019-05-24 00:34:56 +0200

    add ID to list

--- modules/showChannels.js
+++ modules/showChannels.js
@@ -1,10 +1,10 @@
 module.exports = {
     run: client => {
         const channels = Array.from(client.channels.values());
-        console.log("#\tChannel Name\t\t\tUsers");
-        console.log("―".repeat(process.stdout.columns < 48 ? process.stdout.columns : 48));
+        console.log("#\tChannel Name\t\t\tUsers\t\tID");
+        console.log("―".repeat(process.stdout.columns < 60 ? process.stdout.columns : 60));
         for(let i=0;i<channels.length;++i) {
-            console.log(`${i+1}\t${channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length))}\t${channels[i].user_ids.length} Users`);
+            console.log(`${i+1}\t${channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length))}\t${channels[i].user_ids.length} Users \t${channels[i].id}`);
         }
         console.log("\n\nPress CTRL + C anytime to get back to the menu");
     }
