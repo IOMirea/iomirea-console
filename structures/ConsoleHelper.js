b994b9070f8f54b9eca4bed8d32d5c01a0b4af30
Timo <artur.fiedler54@gmail.com>
2019-05-23 16:18:04 +0200

    add ConsoleHelper.reset and iomirea string

--- structures/ConsoleHelper.js
+++ structures/ConsoleHelper.js
@@ -1,3 +1,16 @@
-class ConsoleHelper { }
+class ConsoleHelper {
+    static get iomirea() {
+        return `  _____ ____  __  __ _                
+ |_   _/ __ \\|  \\/  (_)               
+   | || |  | | \\  / |_ _ __ ___  __ _ 
+   | || |  | | |\\/| | | '__/ _ \\/ _\` |
+  _| || |__| | |  | | | | |  __/ (_| |
+ |_____\\____/|_|  |_|_|_|  \\___|\\__,_|\n`;
+    }
+    static reset() {
+        console.clear();
+        console.log(ConsoleHelper.iomirea);
+    }
+}
 
 module.exports = ConsoleHelper;
