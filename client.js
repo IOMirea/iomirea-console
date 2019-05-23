c692335459ad5e110caa527bc4b494d8e203822c
Timo <artur.fiedler54@gmail.com>
2019-05-23 23:39:54 +0200

    add border

--- client.js
+++ client.js
@@ -80,7 +80,7 @@ async function showChannel(channel, state = 0) {
     const messages = state === 0 ? await channel.fetchMessages(true) : channel.messages;
     for(let i=0;i<messages.length;++i) {
         const time = formatDate(Client.getTime(messages[i].id));
-        console.log("[" + time + "]" + (" ".repeat(20 - time.length)) + messages[i].author.name + ">" + (" ".repeat(10 - messages[i].author.name.length))+ messages[i].content.substr(0, process.stdout.columns || 2048));
+        console.log("[" + time + "] " + (" ".repeat(15 - time.length)) + "│ " + messages[i].author.name + (" ".repeat(10 - messages[i].author.name.length))+ "│ " + messages[i].content.substr(0, process.stdout.columns || 2048));
     }
     const spacePad = state === 2 ? 3 : 2;
     console.log(("\n").repeat(messages.length > process.stdout.rows ? 0 : process.stdout.rows - messages.length - spacePad) + (state === 2 ? "[CTRL+C] Back" : "[X] Send Message\t[C] Back to Channel Browser\t[R] Force Reload"));
