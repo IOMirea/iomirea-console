5a64c38a7db914fabd0a1eeae8c0151555a5de1d
Timo <artur.fiedler54@gmail.com>
2019-05-24 00:10:07 +0200

    move to seperate file

---
+++ modules/showMenu.js
@@ -0,0 +1,8 @@
+module.exports = {
+    run: (...args) => {
+        console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
+        console.log("1)\tView Channel");
+        console.log("2)\tAccount Information");
+        console.log("3)\tExit");
+    }
+};
