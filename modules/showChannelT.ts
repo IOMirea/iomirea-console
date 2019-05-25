2b8af60411d6918f8ee68ad9e9d72ae3832d5b11
Timo <artur.fiedler54@gmail.com>
2019-05-25 13:01:26 +0200

    add showChannelT typescript

---
+++ modules/showChannelT.ts
@@ -0,0 +1,21 @@
+import formatDate from './formatDateT';
+import Channel from '../structures/ChannelT';
+import Client from '../structures/Client';
+import Message from '../structures/Message'
+
+export default async function(channel: Channel|string, client: Client, state: number = 0) {
+    if (typeof channel === "string") channel = client.channels.get(channel);
+    const messages: Array<Message> = state === 0 ? await channel.fetchMessages(true) : channel.messages;
+    for(let i: number = 0; i < messages.length; ++i) {
+        const time: string = formatDate(Client.getTime(messages[i].id));
+        console.log("[" + time + "] " + (" ".repeat(15 - time.length)) + "│ " + messages[i].author.name + (" ".repeat(10 - messages[i].author.name.length)) + "│ " + messages[i].content.substr(0, process.stdout.columns || 2048));
+    }
+    const spacePad: number = state === 2 ? 3 : 2;
+    console.log(("\n").repeat(messages.length > process.stdout.rows ? 0 : process.stdout.rows - messages.length - spacePad) + (state === 2 ? "[CTRL+C] Back" : "[X] Send Message\t[C] Back to Channel Browser\t[R] Force Reload"));
+
+    if (state === 2) {
+        console.log("―".repeat(process.stdout.columns));
+        process.stdout.write("> ");
+        if (channel.inputText !== "") process.stdout.write(channel.inputText);
+    }
+}
