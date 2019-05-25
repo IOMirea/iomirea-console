b8c3f5f977c2580f47292b363baad172b7e52d17
Timo <artur.fiedler54@gmail.com>
2019-05-25 17:22:18 +0200

    remove transpiled js files

--- modules/formatDate.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function default_1(date) {
-    if (!(date instanceof Date))
-        date = new Date(date);
-    return (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() + " " + (date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours()) + ":" + (date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes());
-}
-exports.default = default_1;
