1fb7e52d1a2d68e0e9b9bb3bf579c2b2bb70f6ee
Timo <artur.fiedler54@gmail.com>
2019-05-25 12:00:23 +0200

    add getters/setters and constructor

--- structures/User.ts
+++ structures/User.ts
@@ -1,9 +1,35 @@
 export default class User {
-    private id: string;
-    private name: string;
-    private bot: boolean;
+    private _id: string;
+    private _name: string;
+    private _bot: boolean;
 
-    constructor(id, name, bot) {
+    constructor(id: string, name: string, bot: boolean) {
+        this.id = id;
+        this.name = name;
+        this.bot = bot;
+    }
 
+    get id(): string {
+        return this._id;
+    }
+
+    set id(value: string) {
+        this._id = value;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    get bot(): boolean {
+        return this._bot;
+    }
+
+    set bot(value: boolean) {
+        this._bot = value;
     }
 }
