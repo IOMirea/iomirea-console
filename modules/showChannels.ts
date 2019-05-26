5a78bc0030ff65f5b32f40fe151d564569661bf3
Timo <artur.fiedler54@gmail.com>
2019-05-26 21:04:45 +0200

    add showChannels

--- modules/showChannels.ts
+++ modules/showChannels.ts
@@ -4,7 +4,7 @@ import Channel from "../structures/Channel";
 export default function(client: Client) {
     const channels: Array<Channel> = Array.from(client.channels.values());
     console.log("#\tChannel Name\t\t\tUsers\t\tID");
-    console.log("―".repeat(process.stdout.columns < 60 ? process.stdout.columns : 60));
+    console.log("―".repeat(process.stdout.columns));
     for(let i: number = 0;i < channels.length; ++i) {
         console.log(`${i+1}\t${channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length))}\t${channels[i].user_ids.length} Users \t${channels[i].id}`);
     }
