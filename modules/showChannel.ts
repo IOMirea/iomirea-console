764be5a96ea78ab28e0db37f19b5064dfb5159eb
Timo <artur.fiedler54@gmail.com>
2019-05-26 16:13:03 +0200

    update showChannel.ts

--- modules/showChannel.ts
+++ modules/showChannel.ts
@@ -1,4 +1,5 @@
 import formatDate from './formatDate';
+import formatTime from './formatTime';
 import Channel from '../structures/Channel';
 import Client from '../structures/Client';
 import Message from '../structures/Message'
@@ -20,9 +21,12 @@ const texts: any = {
 export default async function (channel: Channel | string, client: Client, state: number = 0) {
     if (typeof channel === "string") channel = client.channels.get(channel);
     const messages: Array<Message> = state === 0 ? await channel.fetchMessages(true) : channel.messages;
+    let date: string;
     for (let i: number = 0; i < messages.length; ++i) {
+        if (i === 0) date = formatDate(Client.getTime(messages[i].id));
         const time: string = formatDate(Client.getTime(messages[i].id));
-        console.log("[" + time + "] " + (" ".repeat(15 - time.length)) + "│ " + messages[i].author.name + " " + (chalk.cyan(messages[i].author.bot ? texts.label.bot : texts.label.user)) + (" ".repeat(10 - messages[i].author.name.length)) + "│ " + messages[i].content.substr(0, process.stdout.columns || 2048));
+        if (date !== time) console.log(' '.repeat(process.stdout.columns / 2 - time.length) + chalk.inverse(date = time));
+        console.log(chalk.magenta(formatTime(Client.getTime(messages[i].id))) + " ".repeat(3) + messages[i].author.name + " " + (chalk.cyan(messages[i].author.bot ? texts.label.bot : texts.label.user)) + (" ".repeat(10 - messages[i].author.name.length)) + " " + messages[i].content.substr(0, process.stdout.columns || 2048));
     }
     let spacePad: number = state === 2 ? 3 : 2;
     console.log(("\n").repeat(messages.length > process.stdout.rows - spacePad ? 0 : process.stdout.rows - messages.length - spacePad) + (state === 2 ? texts.controls.back : `${texts.controls.send}\t${texts.controls.channel_browser}\t${texts.controls.reload}`));
