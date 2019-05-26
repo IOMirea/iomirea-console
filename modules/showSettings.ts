97ed10d16c2a3b186a77f99d0e8f2fe9807444b1
Timo <artur.fiedler54@gmail.com>
2019-05-26 22:54:53 +0200

    use tab instead of space

--- modules/showSettings.ts
+++ modules/showSettings.ts
@@ -1,7 +1,7 @@
 export default function(config: object) {
     console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
-    console.log("1) Change Access Token");
-    console.log("2) Change Language");
-    console.log("3) Change Color Scheme");
-    console.log("4) Back");
+    console.log("1)\tChange Access Token");
+    console.log("2)\tChange Language");
+    console.log("3)\tChange Color Scheme");
+    console.log("4)\tBack");
 }
