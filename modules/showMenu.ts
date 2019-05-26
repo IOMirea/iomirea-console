5f50c253c8bd70b5f47f3aece8e7377046bbb276
Timo <artur.fiedler54@gmail.com>
2019-05-26 22:52:42 +0200

    add settings to menu

--- modules/showMenu.ts
+++ modules/showMenu.ts
@@ -2,5 +2,6 @@ export default function() {
     console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
     console.log("1)\tView Channel");
     console.log("2)\tAccount Information");
-    console.log("3)\tExit");
+    console.log("3)\tSettings");
+    console.log("4)\tExit");
 }
