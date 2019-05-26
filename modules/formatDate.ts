6558c464fd031ee2fce936b06e1a4eccfa2952a3
Timo <artur.fiedler54@gmail.com>
2019-05-26 16:12:31 +0200

    change date format

--- modules/formatDate.ts
+++ modules/formatDate.ts
@@ -1,4 +1,19 @@
 export default function(date: Date|number): string {
     if (!(date instanceof Date)) date = new Date(date);
-    return (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear() + " " + (date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours()) + ":" + (date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes());
+    let month;
+    switch(date.getMonth()) {
+        case 0: month = "January"; break;
+        case 1: month = "Feburary"; break;
+        case 2: month = "March"; break;
+        case 3: month = "April"; break;
+        case 4: month = "May"; break;
+        case 5: month = "June"; break;
+        case 6: month = "July"; break;
+        case 7: month = "August"; break;
+        case 8: month = "September"; break;
+        case 9: month = "October"; break;
+        case 10: month = "November"; break;
+        case 11: month = "December"; break;
+    }
+    return month + " " + date.getDate() + ", " + date.getFullYear();
 }
