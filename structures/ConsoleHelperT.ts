6e376ca8ef756d8854fc84f0c26aedfc53ea9bda
Timo <artur.fiedler54@gmail.com>
2019-05-27 23:29:39 +0200

    remove comments

--- structures/ConsoleHelperT.ts
+++ structures/ConsoleHelperT.ts
@@ -7,10 +7,8 @@ export default class ConsoleHelper {
     static iomirea: string = "  _____ ____  __  __ _\n |_   _/ __ \\|  \\/  (_)\n   | || |  | | \\  / |_ _ __ ___  __ _\n   | || |  | | |\\/| | | '__/ _ \\/ _` |\n  _| || |__| | |  | | | | |  __/ (_| |\n |_____\\____/|_|  |_|_|_|  \\___|\\__,_|\n";
 
     static reset(options: ResetOptions = { border: false, clear: true }): void {
-        //if (typeof options === "object") {
         if (options.border === true) console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
         if (options.clear === true || options.clear === undefined) console.clear();
-        //}
         console.log(ConsoleHelper.iomirea);
     }
 }
