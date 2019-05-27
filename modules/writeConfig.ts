b36660a882256e9ff983ad401086f97d6e5618c1
Timo <artur.fiedler54@gmail.com>
2019-05-27 18:43:05 +0200

    proper callback function and return promise

--- modules/writeConfig.ts
+++ modules/writeConfig.ts
@@ -2,9 +2,12 @@ import Config from '../structures/Config';
 import * as fs from 'fs';
 import chalk from 'chalk';
 
-export default function(config: Config) {
+export default function(config: Config): Promise<Config> {
     const configElements: Array<Array<string>> = Object.entries(config);
-    fs.writeFile("./.config", configElements.map(v => v[0] + "=" + v[1]).join("\n"), (err) => {
-        console.log(chalk.red("An error occurred while trying to write to .config file!"));
+    return new Promise((resolve, reject) => {
+        fs.writeFile("./.config", configElements.map(v => v[0] + "=" + v[1]).join("\n"), (err) => {
+            if (err) reject("An error occurred while trying to write to .config file!");
+            else resolve(config);
+        });
     });
 }
