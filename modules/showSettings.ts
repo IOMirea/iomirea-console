36928dbc46093c0dc1f7867b5ccf7116b35b049f
Timo <artur.fiedler54@gmail.com>
2019-05-27 23:09:14 +0200

    dont draw border manually

--- modules/showSettings.ts
+++ modules/showSettings.ts
@@ -1,5 +1,4 @@
 export default function(config: object) {
-    console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
     console.log("1)\tChange Access Token");
     console.log("2)\tChange Language");
     console.log("3)\tChange Color Scheme");
