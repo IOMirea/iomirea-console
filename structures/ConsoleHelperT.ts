18ffca11a44d950b4951d85903f342fa400d7ce4
Timo <artur.fiedler54@gmail.com>
2019-05-27 23:03:06 +0200

    check if ResetOptions were passed

--- structures/ConsoleHelperT.ts
+++ structures/ConsoleHelperT.ts
@@ -1,8 +1,16 @@
+interface ResetOptions {
+    border?: boolean
+}
+
+
 export default class ConsoleHelper {
     static iomirea: string = "  _____ ____  __  __ _\n |_   _/ __ \\|  \\/  (_)\n   | || |  | | \\  / |_ _ __ ___  __ _\n   | || |  | | |\\/| | | '__/ _ \\/ _` |\n  _| || |__| | |  | | | | |  __/ (_| |\n |_____\\____/|_|  |_|_|_|  \\___|\\__,_|\n";
 
-    static reset(): void {
+    static reset(options?: ResetOptions): void {
         console.clear();
         console.log(ConsoleHelper.iomirea);
+        if (typeof options === "object") {
+            if (options.border === true) console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
+        }
     }
 }
