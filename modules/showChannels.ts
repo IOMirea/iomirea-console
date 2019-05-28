22e675682371e6b90ff701a81a90dfecceb344bd
Timo <artur.fiedler54@gmail.com>
2019-05-28 22:38:19 +0200

    fix weird indentation

--- modules/showChannels.ts
+++ modules/showChannels.ts
@@ -3,7 +3,7 @@ import Channel from '../structures/Channel';
 import chalk from 'chalk';
 
 function print(text:string, state: number, cstate: number): string {
-    return (state === cstate ? chalk.inverse(text) : text).replace(/\t/g, " ".repeat(8));
+    return (state === cstate ? chalk.inverse(text) : text);
 }
 
 export default function(client: Client, state: number) {
@@ -11,7 +11,9 @@ export default function(client: Client, state: number) {
     console.log("#\tChannel Name\t\t\tUsers\t\tID");
     console.log("―".repeat(process.stdout.columns));
     for(let i: number = 0;i < channels.length; ++i) {
-        console.log(print(`${i+1}\t${channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length))}\t${channels[i].user_ids.length} Users \t${channels[i].id}`, state, i + 1));
+        const name: string = channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length));
+        const users = channels[i].user_ids.length + " Users";
+        console.log(print(`${i+1}${" ".repeat(8)}${name}${" ".repeat(35 - (name.endsWith("...") ? 27 : name.length))}${users}${" ".repeat(16 - users.length)}${channels[i].id}`, state, i + 1));
     }
     console.log("\n\nPress CTRL + C anytime to get back to the menu");
     console.log("(" + state + ")")
