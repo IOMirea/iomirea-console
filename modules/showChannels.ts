549bea2d53d7dc329a97e1b9473ea1d75f0d3be8
Timo <artur.fiedler54@gmail.com>
2019-05-28 17:57:53 +0200

    accept state parameter

--- modules/showChannels.ts
+++ modules/showChannels.ts
@@ -1,7 +1,7 @@
 import Client from '../structures/Client';
 import Channel from "../structures/Channel";
 
-export default function(client: Client) {
+export default function(client: Client, state: number) {
     const channels: Array<Channel> = Array.from(client.channels.values());
     console.log("#\tChannel Name\t\t\tUsers\t\tID");
     console.log("―".repeat(process.stdout.columns));
@@ -9,4 +9,5 @@ export default function(client: Client) {
         console.log(`${i+1}\t${channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length))}\t${channels[i].user_ids.length} Users \t${channels[i].id}`);
     }
     console.log("\n\nPress CTRL + C anytime to get back to the menu");
+    console.log("(" + state + ")")
 }
