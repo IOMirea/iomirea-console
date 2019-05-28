c4b64d53b244725efb28f887795699f7ac478e5e
Timo <artur.fiedler54@gmail.com>
2019-05-28 18:08:19 +0200

    implement consoleselector

--- modules/showChannels.ts
+++ modules/showChannels.ts
@@ -1,12 +1,17 @@
 import Client from '../structures/Client';
-import Channel from "../structures/Channel";
+import Channel from '../structures/Channel';
+import chalk from 'chalk';
+
+function print(text:string, state: number, cstate: number): string {
+    return (state === cstate ? chalk.inverse(text) : text).replace(/\t/g, " ".repeat(8));
+}
 
 export default function(client: Client, state: number) {
     const channels: Array<Channel> = Array.from(client.channels.values());
     console.log("#\tChannel Name\t\t\tUsers\t\tID");
     console.log("―".repeat(process.stdout.columns));
     for(let i: number = 0;i < channels.length; ++i) {
-        console.log(`${i+1}\t${channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length))}\t${channels[i].user_ids.length} Users \t${channels[i].id}`);
+        console.log(print(`${i+1}\t${channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length))}\t${channels[i].user_ids.length} Users \t${channels[i].id}`, state, i + 1));
     }
     console.log("\n\nPress CTRL + C anytime to get back to the menu");
     console.log("(" + state + ")")
