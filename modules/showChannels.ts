15ea39bcac17bb94c05328d82fcffa75a964f923
Timo <artur.fiedler54@gmail.com>
2019-05-29 18:48:28 +0200

    use ConsoleSelector#log instead of print fn

--- modules/showChannels.ts
+++ modules/showChannels.ts
@@ -3,10 +3,6 @@ import Channel from '../structures/Channel';
 import ConsoleSelector from '../structures/ConsoleSelector';
 import chalk from 'chalk';
 
-function print(text:string, state: number, cstate: number): string {
-    return (state === cstate ? chalk.inverse(text) : text);
-}
-
 export default function(client: Client, cselector: ConsoleSelector) {
     if (cselector.state === undefined) cselector.state = 1;
     const channels: Array<Channel> = Array.from(client.channels.values());
@@ -15,8 +11,7 @@ export default function(client: Client, cselector: ConsoleSelector) {
     for(let i: number = 0;i < channels.length; ++i) {
         const name: string = channels[i].name.substr(0, 24) + (channels[i].name.length > 24 ? "..." : ' '.repeat(24 - channels[i].name.length));
         const users = channels[i].user_ids.length + " Users";
-        console.log(print(`${i+1}${" ".repeat(8)}${name}${" ".repeat(35 - (name.endsWith("...") ? 27 : name.length))}${users}${" ".repeat(16 - users.length)}${channels[i].id}`, cselector.state, i + 1));
-
+        cselector.log(`${i+1}${" ".repeat(8)}${name}${" ".repeat(35 - (name.endsWith("...") ? 27 : name.length))}${users}${" ".repeat(16 - users.length)}${channels[i].id}`, i);
     }
     console.log("\n\nPress CTRL + C anytime to get back to the menu");
 }
