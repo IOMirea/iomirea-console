a2944a9e41036bb853d9be9be8760ab6f5f06385
Timo <artur.fiedler54@gmail.com>
2019-06-23 23:13:03 +0200

    200 iq

--- modules/showSettings.ts
+++ modules/showSettings.ts
@@ -6,6 +6,6 @@ export default function(config: object, cselector: ConsoleSelector) {
 
     for (let i: number = 0; i < SettingsEntries.length; ++i) {
         // TODO: use i + 1 instead of just i
-        // cselector.log(i + ")" + " ".repeat(8) + SettingsEntries[i], i);
+        cselector.log(i + ")" + " ".repeat(8) + SettingsEntries[i], i);
     }
 }
