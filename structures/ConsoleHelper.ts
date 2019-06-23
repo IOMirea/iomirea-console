04050ae2779ad0ceaadb2cc3d6e48b3eaf957538
Timo <artur.fiedler54@gmail.com>
2019-06-23 23:39:49 +0200

    add version to ConsoleHelper.iomirea

--- structures/ConsoleHelper.ts
+++ structures/ConsoleHelper.ts
@@ -4,7 +4,7 @@ interface ResetOptions {
 }
 
 export default class ConsoleHelper {
-    static iomirea: string = "  _____ ____  __  __ _\n |_   _/ __ \\|  \\/  (_)\n   | || |  | | \\  / |_ _ __ ___  __ _\n   | || |  | | |\\/| | | '__/ _ \\/ _` |\n  _| || |__| | |  | | | | |  __/ (_| |\n |_____\\____/|_|  |_|_|_|  \\___|\\__,_|\n";
+    static iomirea: string = "  _____ ____  __  __ _\n |_   _/ __ \\|  \\/  (_)\n   | || |  | | \\  / |_ _ __ ___  __ _\n   | || |  | | |\\/| | | '__/ _ \\/ _` |\n  _| || |__| | |  | | | | |  __/ (_| |\n |_____\\____/|_|  |_|_|_|  \\___|\\__,_| v1.0\n";
 
     static reset(options: ResetOptions = { border: false, clear: true }): void {
         if (options.border === true) console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
