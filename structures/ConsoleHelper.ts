b2505db83b952922ff605733571946f3a66b9bd5
Timo <artur.fiedler54@gmail.com>
2019-06-23 22:28:28 +0200

    rename consolehelper

---
+++ structures/ConsoleHelper.ts
@@ -0,0 +1,14 @@
+interface ResetOptions {
+    border?: boolean,
+    clear?: boolean
+}
+
+export default class ConsoleHelper {
+    static iomirea: string = "  _____ ____  __  __ _\n |_   _/ __ \\|  \\/  (_)\n   | || |  | | \\  / |_ _ __ ___  __ _\n   | || |  | | |\\/| | | '__/ _ \\/ _` |\n  _| || |__| | |  | | | | |  __/ (_| |\n |_____\\____/|_|  |_|_|_|  \\___|\\__,_|\n";
+
+    static reset(options: ResetOptions = { border: false, clear: true }): void {
+        if (options.border === true) console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
+        if (options.clear === true || options.clear === undefined) console.clear();
+        console.log(ConsoleHelper.iomirea);
+    }
+}
