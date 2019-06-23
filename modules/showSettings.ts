f224b46f5d81fc1162017b98183adbade3bf049f
Timo <artur.fiedler54@gmail.com>
2019-06-23 22:53:09 +0200

    add comment so I wont forget it

--- modules/showSettings.ts
+++ modules/showSettings.ts
@@ -5,6 +5,7 @@ export default function(config: object, cselector: ConsoleSelector) {
     if (cselector.state === undefined) cselector.state = 1;
 
     for (let i: number = 0; i < SettingsEntries.length; ++i) {
-        cselector.log(i + ")" + " ".repeat(8) + SettingsEntries[i], i);
+        // TODO: use i + 1 instead of just i
+        // cselector.log(i + ")" + " ".repeat(8) + SettingsEntries[i], i);
     }
 }
