0f8c433504d588b3201db0b282ec9538e4f5af3b
Timo <artur.fiedler54@gmail.com>
2019-06-24 11:40:03 +0200

    check if recipients are only numbers

--- modules/createChannel.ts
+++ modules/createChannel.ts
@@ -6,9 +6,11 @@ export default function(rl: Interface) {
     // Use arrow function in callback to keep context as client instance
     rl.question(chalk.green("Channel Name: "), name => {
         rl.question(chalk.green("Recipient IDs ") + chalk.yellow("(optional)") + chalk.green(" - seperate with comma: "), recipients => {
+            const user_ids = recipients.trim().split(/, */);
+            if (user_ids.some(v => !/^\d+$/.test(v))) return console.log(chalk.red("User IDs are only numbers!"));
             Channel.create(this, {
                 name,
-                user_ids: recipients.trim().split(/, */)
+                user_ids
             }).then((c: Channel) => {
                 const channel: Channel = new Channel(c.id, c.name, c.owner_id, c.user_ids, c.pinned_ids, this);
                 this.channels.set(channel.id, channel);
