7d1969773f56cacd3a25534d6301ee874efd23c8
Timo <artur.fiedler54@gmail.com>
2019-06-24 13:00:13 +0200

    bind this context

--- modules/showAccount.ts
+++ modules/showAccount.ts
@@ -1,8 +1,6 @@
-import Client from '../structures/Client';
-
-export default function(client: Client): void {
-    console.log(`ID\t\t${client.user.id}`);
-    console.log(`Username\t${client.user.name}`);
-    console.log(`E-Mail:\t\t${client.user.email}`);
+export default function(): void {
+    console.log(`ID\t\t${this.user.id}`);
+    console.log(`Username\t${this.user.name}`);
+    console.log(`E-Mail:\t\t${this.user.email}`);
     console.log("(Press any key to go back)");
 }
