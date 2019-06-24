a674093c713100ff17bdde013b00b8cfb820dadd
Timo <artur.fiedler54@gmail.com>
2019-06-24 10:59:54 +0200

    implement createChannel functionality

--- modules/createChannel.ts
+++ modules/createChannel.ts
@@ -10,7 +10,9 @@ export default function(rl: Interface) {
                 name,
                 user_ids: recipients.trim().split(/, */)
             }).then((c: Channel) => {
-                this.channels.set(c.id, c);
+                const channel: Channel = new Channel(c.id, c.name, c.owner_id, c.user_ids, c.pinned_ids, this);
+                this.channels.set(channel.id, channel);
+                console.log(chalk.green("Successfully created channel."));
             }).catch(e => {
                 console.log(chalk.red("Could not create channel: " + e));
             });
