4cf92deeac8423b3c1b0f37eea355a1e8d071562
Timo <artur.fiedler54@gmail.com>
2019-06-24 12:00:10 +0200

    add rlStates constant

--- structures/Constants.ts
+++ structures/Constants.ts
@@ -4,3 +4,21 @@ export const SettingsEntries: Array<string> = [
     "Change Color Scheme",
     "Back"
 ];
+
+
+export const rlStates: any = {
+    "ACCESS_TOKEN_INPUT": -1,
+    "MENU_SELECTION": 0,
+    "MENU_VIEW_CHANNEL": 0.1,
+    "MENU_ACCOUNT_INFO": 0.2,
+    "MENU_SETTINGS": 0.3,
+    "MENU_EXIT": 0.4,
+    "VIEW_CHANNELS": 1,
+    "ACCOUNT_INFO": 2,
+    "CHANNEL_BROWSER": 3,
+    "CHANNEL_BROWSER_SEND_MESSAGE": 4,
+    "SETTINGS": 5,
+    "SETTINGS_ACCESS_TOKEN": 6,
+    "SETTINGS_LANGUAGES": 7,
+    "CREATE_CHANNEL": 8
+};
