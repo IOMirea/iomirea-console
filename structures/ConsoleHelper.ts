970b4c1f4127b0e7a504b03395677876feb28d69
Timo <artur.fiedler54@gmail.com>
2019-06-25 12:52:28 +0200

    add jsdoc for ConsoleHelper

--- structures/ConsoleHelper.ts
+++ structures/ConsoleHelper.ts
@@ -6,6 +6,13 @@ interface ResetOptions {
 export default class ConsoleHelper {
     static iomirea: string = "  _____ ____  __  __ _\n |_   _/ __ \\|  \\/  (_)\n   | || |  | | \\  / |_ _ __ ___  __ _\n   | || |  | | |\\/| | | '__/ _ \\/ _` |\n  _| || |__| | |  | | | | |  __/ (_| |\n |_____\\____/|_|  |_|_|_|  \\___|\\__,_| v1.0\n";
 
+    /**
+     * Resets the console (clears stdout) and prints IOMirea ascii art
+     *
+     * @static
+     * @param {ResetOptions?} options Options for clearing (border: false, clear: true by default)
+     * @returns {undefined}
+     */
     static reset(options: ResetOptions = { border: false, clear: true }): void {
         if (options.border === true) console.log("―".repeat(process.stdout.columns < 50 ? process.stdout.columns : 50));
         if (options.clear === true || options.clear === undefined) console.clear();
