fa9249b0fce624b0abe65fc283df3933395572df
Timo <artur.fiedler54@gmail.com>
2019-06-25 11:39:13 +0200

    specify return type

--- modules/createChannel.ts
+++ modules/createChannel.ts
@@ -8,7 +8,7 @@ import chalk from "chalk";
  * @param {Interface} rl Readline Interface
  * @returns {undefined}
  */
-export default function(rl: Interface) {
+export default function(rl: Interface): void {
     // Use arrow function in callback to keep context as client instance
     rl.question(chalk.green("Channel Name: "), name => {
         rl.question(chalk.green("Recipient IDs ") + chalk.yellow("(optional)") + chalk.green(" - seperate with comma: "), recipients => {
