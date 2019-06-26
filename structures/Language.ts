cac60a8b084e58851aeed5660b776720f61e5dad
Timo <artur.fiedler54@gmail.com>
2019-06-26 19:35:44 +0200

    add Language class

---
+++ structures/Language.ts
@@ -0,0 +1,41 @@
+import * as chalk from 'chalk';
+
+export interface TextObject {
+    t: string,
+    c?: string
+}
+
+export interface Text {
+    CHANNEL_NAME_INPUT: TextObject,
+    RECIPIENTS_ID_INPUT: TextObject,
+    USERID_NOT_INT: TextObject,
+    CHANNEL_CREATE_S: TextObject,
+    CHANNEL_CREATE_E: TextObject
+}
+
+const colorRegex: RegExp = /<(\w{1,10})>([^<]+)<\/\w{1,10}>/;
+
+export default class Language {
+    public texts: Text;
+
+    constructor(texts: Text) {
+        this.texts = texts;
+
+        // Define `f` getter for each text
+        for (const [k, v] of Object.entries(this.texts)) {
+            Object.defineProperty(this.texts[k], "f", {
+                get() {
+                    return chalk[v.c](v.t);
+                }
+            });
+        }
+    }
+
+    escape(textConstant: string, replaceObj: any): string {
+        let r: string = this.texts[textConstant].t;
+        for (const [k, v] of Object.entries(replaceObj)) {
+            r = r.replace(new RegExp("{" + k + "}", "g"), v.toString());
+        }
+        return r;
+    }
+}
