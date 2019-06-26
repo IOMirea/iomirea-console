2a1249978ea75e6098b908ad4d4550adad894e50
Timo <artur.fiedler54@gmail.com>
2019-06-26 20:10:51 +0200

    use translations in formatDate

--- modules/formatDate.ts
+++ modules/formatDate.ts
@@ -6,20 +6,6 @@
  */
 export default function(date: Date|number): string {
     if (!(date instanceof Date)) date = new Date(date);
-    let month;
-    switch(date.getMonth()) {
-        case 0: month = "January"; break;
-        case 1: month = "Feburary"; break;
-        case 2: month = "March"; break;
-        case 3: month = "April"; break;
-        case 4: month = "May"; break;
-        case 5: month = "June"; break;
-        case 6: month = "July"; break;
-        case 7: month = "August"; break;
-        case 8: month = "September"; break;
-        case 9: month = "October"; break;
-        case 10: month = "November"; break;
-        case 11: month = "December"; break;
-    }
+    let month = this.language.texts["MONTH_" + (date.getMonth() + 1)].t;
     return month + " " + date.getDate() + ", " + date.getFullYear();
 }
